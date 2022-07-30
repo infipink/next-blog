b8cfdcc09e04efbc805425ce0ad739ef24f8457a
wzb <qaz675323@163.com>
2022-07-30 22:12:47 +0800

    feat: add tailwindCss

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Script from 'next/script'
+import Script from 'next/script';
 import styles from './layout.module.css';
 import Link from 'next/link';
 import utilStyles from '../styles/utils.module.css';
@@ -11,41 +11,33 @@ export const siteTitle = '难得有趣';
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="前端学习" />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-      </Head>
-      <header className={styles.header}>
-        {/* {home && (
-          <>
-            <Image
-              priority
-              src={profilePic}
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        )} */}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← 回到首页</a>
-          </Link>
+    <div className="w-screen h-screen relative">
+      <div className="sticky top-0 left-0 w-full border-b border-orange-600">
+        <div className="max-w-7xl mx-auto py-0.5">
+        头部
         </div>
-      )}
+      </div>
+      <div className={styles.container}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="前端学习" />
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle,
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+        </Head>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← 回到首页</a>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- posts/ci-song.md
+++ posts/ci-song.md
@@ -5,9 +5,11 @@ date: '2022-07-30'
 
 <p align="center">定风波·南海归赠王定国侍人寓娘</p>
 
-<p algin="center">作者：苏轼</p>
+<p align="center">作者：苏轼</p>
 
 <p align="center">常羡人间琢玉郎，天应乞与点酥娘。尽道清歌传皓齿，风起，雪飞炎海变清凉。</p>
 
 <p align="center">万里归来颜愈少，微笑，笑时犹带岭梅香。试问岭南应不好，却道，<span style="color: green">此心安处是吾乡</span>。</p>
+
+------
 
