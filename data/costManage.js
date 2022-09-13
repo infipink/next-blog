bfdcecf76e12dea8f5afed038974315b8ac72d01
infipink <qaz675323@163.com>
2022-09-13 23:42:14 +0800

    add：cost

---
+++ data/costManage.js
@@ -0,0 +1,13 @@
+const steps = [
+    '规划成本',
+    '估算成本',
+    '制定预算',
+    '控制成本',
+];
+
+const infos = [];
+// {name: '', content: ['',]},
+
+const title = '项目成本管理';
+
+export default [steps, infos, title];
