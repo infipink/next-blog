7d9edd20b7852817ae41b02958d9bf867d4e1bff
wzb <qaz675323@163.com>
2022-09-15 16:12:57 +0800

    add：quality

--- data/index.js
+++ data/index.js
@@ -2,5 +2,6 @@ import overallData from './overallManage';
 import scopeData from './scopeManage';
 import progressData from './progressManage';
 import costData from './costManage';
+import qualityData from './qualityManage';
 
-export default [overallData, scopeData, progressData, costData];
+export default [overallData, scopeData, progressData, costData, qualityData];
